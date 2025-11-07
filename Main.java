9546b4f3c64d256e2ede7a25dcb2724da774c482
Adrian Padilla <apad99@pop-os.lan>
2025-11-07 11:39:11 -0600

    finish player side

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        QuestionService test = new QuestionService();
+
+        test.playQuiz();
+        test.printScore();
+    }
+}
